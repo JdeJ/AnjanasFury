5a35e06caafb9a774499117d2e83ec447d2b0929
JdeJ <jorgedjuana@gmail.com>
2019-01-09 22:39:58 +0100

    Adds items improve performance

--- src/assets.js
+++ src/assets.js
@@ -16,23 +16,10 @@ const images = generateImagesObject('img/stage1.png', 'img/stage2.png',
                                     'img/haggar-player.png', 'img/haggar-select.png',
                                     'img/items.png'); 
 
-//Items sprites objects
-var rewardsSprites = {
-  radio: new Sprite('img/items.png',{x:115,y:7},{width:29, height:16},{width:69, height:38},0,1,false),
-  dollar: new Sprite('img/items.png',{x:164,y:7},{width:32, height:11},{width:63, height:22},0,1,false),
-  gold: new Sprite('img/items.png',{x:9,y:7},{width:32, height:12},{width:69, height:26},0,1,false),
-  hat: new Sprite('img/items.png',{x:56,y:7},{width:24, height:13},{width:57, height:34},0,1,false),
-  hamburguer: new Sprite('img/items.png',{x:164,y:33},{width:31, height:15},{width:55, height:26},0,1,false),
-  barbeque: new Sprite('img/items.png',{x:115,y:33},{width:32, height:22},{width:49, height:34},0,1,false),
-  pizza: new Sprite('img/items.png',{x:9,y:33},{width:30, height:14},{width:62, height:29},0,1,false),
-  pineapple: new Sprite('img/items.png',{x:56,y:33},{width:32, height:16},{width:55, height:28},0,1,false),
-};
+//Items arrays
+var rewardsArray = ['radio', 'dollar', 'gold', 'hat', 'hamburguer', 'barbeque', 'pizza', 'pineapple'];
 
-var obstaclesSprites = {
-  bidon: new Sprite('img/items.png',{x:30,y:177},{width:32, height:62},{width:115, height:222},0,2,false),
-  tyres: new Sprite('img/items.png',{x:122,y:178},{width:34, height:52},{width:146, height:222},0,1,false),
-  box: new Sprite('img/items.png',{x:197,y:172},{width:48, height:80},{width:133, height:222},0,2,false),
-};
+var obstaclesArray = ['bidon', 'tyres', 'box'];
 
 //Enemies array
 var enemiesArray = [];
--- src/item.js
+++ src/item.js
@@ -3,9 +3,9 @@ class Item{
   constructor (phaseXLimits, phaseYLimits){
     this.phaseXLimits = phaseXLimits; //Object with the min and max X position of the phase
     this.phaseYLimits = phaseYLimits; //Object with the min and max Y position of the phase
-    this.obstacleSprite = this.randomSprite(obstaclesSprites); 
-    this.rewardSprite = this.randomSprite(rewardsSprites);
-    this.sprite = this.obstacleSprite; //Sprite drawed in canvas
+    this.obstacleSprite = this.randomSprite(obstaclesArray); 
+    this.rewardSprite = this.randomSprite(rewardsArray);
+    this.sprite = this.obstacleSprite; //Sprite default drawed in canvas
     this.x = 575; //object random x position
     this.y = undefined; //Object random y position
     this.health = 250;
@@ -15,9 +15,46 @@ class Item{
   }
 
   //random obstacle sprite
-  randomSprite (spritesObj){
-    let keys = Object.keys(spritesObj);
-    return spritesObj[keys[Math.floor(Math.random() * keys.length)]];
+  randomSprite (itemArray){
+
+    let item = itemArray[Math.floor(Math.random() * itemArray.length)];
+
+    switch (item){
+      case 'radio':
+        return new Sprite('img/items.png',{x:115,y:7},{width:29, height:16},{width:69, height:38},0,1,false);
+        break;
+      case 'dollar':
+      return new Sprite('img/items.png',{x:164,y:7},{width:32, height:11},{width:63, height:22},0,1,false);
+        break;
+      case 'gold':
+        return new Sprite('img/items.png',{x:9,y:7},{width:32, height:12},{width:69, height:26},0,1,false);
+        break;
+      case 'hat':
+        return new Sprite('img/items.png',{x:56,y:7},{width:24, height:13},{width:57, height:34},0,1,false);
+        break;
+      case 'hamburguer':
+        return new Sprite('img/items.png',{x:164,y:33},{width:31, height:15},{width:55, height:26},0,1,false);
+        break;
+      case 'barbeque':
+        return new Sprite('img/items.png',{x:115,y:33},{width:32, height:22},{width:49, height:34},0,1,false);
+        break;
+      case 'pizza':
+        return new Sprite('img/items.png',{x:9,y:33},{width:30, height:14},{width:62, height:29},0,1,false);
+        break;
+      case 'pineapple':
+        return new Sprite('img/items.png',{x:56,y:33},{width:32, height:16},{width:55, height:28},0,1,false);
+        break;
+      case 'bidon':
+        return new Sprite('img/items.png',{x:30,y:177},{width:32, height:62},{width:115, height:222},0,2,false);
+        break;
+      case 'tyres':
+        return new Sprite('img/items.png',{x:122,y:178},{width:34, height:52},{width:146, height:222},0,1,false);
+        break;
+      case 'box':
+        return new Sprite('img/items.png',{x:197,y:172},{width:48, height:80},{width:133, height:222},0,2,false);
+        break;
+      }
+
   }
 
   //random obstacle position
