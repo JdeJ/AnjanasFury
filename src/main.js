3374c3ed87ce8e42175122361e84ca28c65a5ee4
JdeJ <jorgedjuana@gmail.com>
2019-01-14 09:30:28 +0100

    Adds player custom game over screen

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ document.onload = function () {
   const ctx = canvas.getContext('2d');
   const crt = document.querySelector(".crt");
 
-  gameInit('haggar');
+  gameInit('cody');
   //playerSelect();
   //gameOverCB();
 
